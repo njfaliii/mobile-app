77525289f6dd7983cacd06f90f4d75916015240b
njfaliii <158143856+njfaliii@users.noreply.github.com>
2024-10-01 10:27:18 +0500

    Add files via upload

---
+++ lab3.dart
@@ -0,0 +1,182 @@
+// import 'dart:io';
+// void main() {
+// print("Enter your name?");
+
+// String? name = stdin.readLineSync();
+// print("Hello, $name! \nWelcome to
+// Pakistan!!");
+// }
+
+
+// import 'dart:io';
+// void main(){
+//   print("Enter your fav number:");
+//   int?n =int.parse(stdin.readLineSync()!);
+//   print("your fav number is $n");
+// }
+
+
+// import 'dart:io';
+// void main(){
+//   print("Welcome to Pakistan!");
+//   stdout.write("Welcome to Pakistan!");
+// }
+
+// import 'dart:io';
+// void main(){
+//   print("Welcome to Pakistan!");
+//   stdout.write("Wlecome to Pakistan!");
+//   String s =stdin.readLineSync()!;
+//   print('$s');
+//   stdout.write(s);
+// }
+
+
+
+
+// import 'dart:io';
+// void main() {
+// List tst = List.filled(3, null);
+// tst[0] = 'One';
+// tst[1] = 'Two';
+// tst[2] = 'Three';
+// print(tst);
+// print(tst[0]);
+// }
+
+// import 'dart:io';
+// void main() {
+// dynamic mapType = new Map();
+// mapType['First'] = 'Love';
+// mapType['Second'] = 'For';
+// mapType['Third'] = 'Pakistan';
+// print(mapType);
+// stdout.write(mapType['Third']);}
+
+// import 'dart:io';
+// void main() {
+// double n = 8;
+// int t = 8;
+// num s = 8;
+// print(n.hashCode);
+// print(t.isFinite);
+// print(identityHashCode(s));
+// double result = 5 / 0; 
+// print(result); 
+// print(result.isInfinite);
+// double nanResult = 0 / 0; 
+// print(nanResult); 
+// print(nanResult.isNaN);
+// int g = 5;
+// print(g.isNegative);
+// print(g.sign);
+// print(g.isEven);
+// print(g.isOdd);}
+
+
+// import 'dart:io';
+// void main() {
+// String st = "Hello";
+// String st1 = "Pakistan";
+// var convar = "$st my name is $st1";
+// print(convar);
+// }
+
+
+// import 'dart:io';
+// void main() {
+// var str = 'Abcd';
+// var str1 = 'abcd';
+// if (str == str1) {
+// print('True');
+//     }
+//     else{
+//       print('false');
+//     }
+//    }
+
+
+// import 'dart:io';
+// void main() {
+// var f = r'This \n is a raw string';
+// var s = 'This \n is a raw string';
+// print(f); 
+// print(s); 
+// escape character}
+
+
+// Set<int> mySet = {};
+// print(mySet);
+// Set<String> fruitSet = {'apple',
+// 'banana', 'orange'};
+
+
+
+
+// var lst = ['Apple', 'For', 'Apple'];
+// print ("Output of the list is: $lst");
+// var sets = <String>{'Apple', 'For',
+// 'Apple'};
+// print ("Output of the set is: $sets");
+
+
+
+
+// fruitSet.add('grape');
+// fruitSet.addAll({'kiwi', 'mango', 'mango'});
+// print(fruitSet);
+
+// Set<String> sets = {'Apple', 'For', 'mango'};
+// print ("Using forEach in set:");
+// sets.forEach((element) {
+// if (element == "Apple") {
+// print("Found");
+// } else {
+// print("Not Found");}});
+
+
+
+// Set<String> tst = {'Hello Students',"Teachers","Admin1","Admin2","Admin3"};
+// List<String> tst_lst = tst.toList();
+
+
+
+
+
+
+
+
+
+
+
+// Map<String, int> myMap = {
+//   'apple': 3,
+//   'banana': 5,
+//   'orange': 2,
+// };
+// print(myMap['apple']); 
+
+
+
+
+
+// import 'dart:io';
+// void main(){
+// dynamic mapType= new Map();
+// mapType['First'] = 'najaf';
+// mapType['second'] = 'ali';
+// stdout.write(mapType['First']);
+// print(mapType);
+// }
+
+
+
+
+// void main(){
+//   dynamic mapType= new Map();
+//   mapType['First'] = 'najaf';
+//   mapType['hello world'] = 'hello';
+//   print(mapType);
+// }
+
+
