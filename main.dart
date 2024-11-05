3385aa2c3a95c7e6ee48f6e2a8230734a2cd1cc6
njfaliii <158143856+njfaliii@users.noreply.github.com>
2024-11-06 00:01:56 +0500

    assignment3

--- main.dart
+++ main.dart
@@ -9,91 +9,131 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My App'),
-        ),
-        body: SingleChildScrollView(
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        color: const Color.fromARGB(255, 199, 111, 111),
-                      ),
-                      Container(
-                        width: 100,
-                        height: 100,
-                        color: const Color.fromARGB(255, 208, 201, 0),
-                      ),
-                      Container(
-                        width: 100,
-                        height: 100,
-                        color: const Color.fromARGB(255, 4, 142, 188),
-                      ),
-                      Container(
-                        width: 100,
-                        height: 100,
-                        color: const Color.fromARGB(255, 95, 6, 43),
-                      ),
-                      Container(
-                      width: 100,
-                      height: 100,
-                      color: const Color.fromARGB(255, 76, 185, 224),
+              Container(
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
                     ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: const Color.fromARGB(255, 211, 23, 208),
-                    )
-                    ],
-                  ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'RADO Watches',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: const Color.fromARGB(255, 14, 15, 14),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'RADO is a Swiss luxury watchmaking company known for its innovative designs and high-quality timepieces. Since 1917, RADO has set new trends in the watch industry, blending craftsmanship with cutting-edge technology and materials.',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
                 ),
               ),
-              Column(
+              SizedBox(height: 16),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: const Color.fromARGB(255, 27, 120, 11),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            'https://example.com/rado_watch.jpg', // Replace with an actual image URL of a RADO watch
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
                   ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: const Color.fromARGB(255, 106, 108, 32),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: const Color.fromARGB(255, 42, 22, 144),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: const Color.fromARGB(255, 96, 28, 48),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: const Color.fromARGB(255, 27, 120, 11),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: const Color.fromARGB(255, 76, 185, 224),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: const Color.fromARGB(255, 211, 23, 208),
+                  SizedBox(width: 16),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Innovative Timepieces',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'RADO’s dedication to design and innovation has positioned it as a leader in luxury watchmaking. With unique materials and advanced technology, RADO watches are known for their durability and aesthetic appeal, catering to watch enthusiasts worldwide.',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
-              )
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.home),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.contact_mail),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.phone),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                    iconSize: 30,
+                    icon: Image.network(
+                      'https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg',
+                    ),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    iconSize: 30,
+                    icon: Image.network(
+                      'https://upload.wikimedia.org/wikipedia/commons/0/0b/Google_Logo_2015.png',
+                    ),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    iconSize: 30,
+                    icon: Image.network(
+                      'https://upload.wikimedia.org/wikipedia/en/6/60/Twitter_Logo_as_of_2021.svg',
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
             ],
           ),
         ),
